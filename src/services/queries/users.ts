124824a8b8302e78dad6e8b63344149c332725d2
leobewater <mmbee888@gmail.com>
2024-03-16 15:38:09 -0400

    store new username and password in redis

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,3 +1,5 @@
+import { usersKey } from '$services/keys';
+import { client } from '$services/redis';
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 
@@ -5,4 +7,18 @@ export const getUserByUsername = async (username: string) => {};
 
 export const getUserById = async (id: string) => {};
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+export const createUser = async (attrs: CreateUserAttrs) => {
+  const id = genId();
+
+  // Store user to Redis
+  await client.hSet(usersKey(id), serialize(attrs));
+
+  return id;
+};
+
+const serialize = (user: CreateUserAttrs) => {
+  return {
+    username: user.username,
+    password: user.password
+  }
+}
