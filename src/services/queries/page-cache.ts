3f66f1884d702327c54d2fc3cc2af0f294223523
leobewater <mmbee888@gmail.com>
2024-03-16 10:08:31 -0400

    set pages to cache

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -6,9 +6,17 @@ export const getCachedPage = (route: string) => {
 	if (cacheRoutes.includes(route)) {
 		// get cached route from Redis
 		return client.get('pagecache#' + route);
-  }
-  
-  return null;
+	}
+
+	return null;
 };
 
-export const setCachedPage = (route: string, page: string) => {};
+export const setCachedPage = (route: string, page: string) => {
+  if (cacheRoutes.includes(route)) {
+    // set key and value pair with expiration for 2 seconds
+    return client.set('pagecache#' + route, page, {
+      EX: 2
+    });
+  }
+  return null;
+};
