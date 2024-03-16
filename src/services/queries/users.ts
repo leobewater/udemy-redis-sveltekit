0dbb32a6da7ff239fb37f5c958c951c02b65a654
leobewater <mmbee888@gmail.com>
2024-03-16 15:41:48 -0400

    get user by id and deserialize it

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -5,20 +5,32 @@ import { genId } from '$services/utils';
 
 export const getUserByUsername = async (username: string) => {};
 
-export const getUserById = async (id: string) => {};
+export const getUserById = async (id: string) => {
+	const user = await client.hGetAll(usersKey(id));
+
+	return deserialize(id, user);
+};
 
 export const createUser = async (attrs: CreateUserAttrs) => {
-  const id = genId();
+	const id = genId();
 
-  // Store user to Redis
-  await client.hSet(usersKey(id), serialize(attrs));
+	// Store user to Redis
+	await client.hSet(usersKey(id), serialize(attrs));
 
-  return id;
+	return id;
 };
 
 const serialize = (user: CreateUserAttrs) => {
-  return {
-    username: user.username,
-    password: user.password
-  }
-}
+	return {
+		username: user.username,
+		password: user.password
+	};
+};
+
+const deserialize = (id: string, user: { [key: string]: string }) => {
+	return {
+		id,
+		username: user.username,
+		password: user.password
+	};
+};
