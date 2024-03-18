62d4a7eb3f39bebb3f012da2d8125e1b5d673348
leobewater <mmbee888@gmail.com>
2024-03-18 10:20:16 -0400

    add create item to redis

--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -1,7 +1,19 @@
+import { itemsKey } from '$services/keys';
+import { serialize } from '$services/queries/items/serialize';
+import { client } from '$services/redis';
 import type { CreateItemAttrs } from '$services/types';
+import { genId } from '$services/utils';
 
 export const getItem = async (id: string) => {};
 
 export const getItems = async (ids: string[]) => {};
 
-export const createItem = async (attrs: CreateItemAttrs, userId: string) => {};
+export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
+	const id = genId();
+
+	const serialized = serialize(attrs);
+	
+  await client.hSet(itemsKey(id), serialized);
+	
+  return id;
+};
