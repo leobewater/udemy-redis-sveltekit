7b5601ebcda4e25056264e1fe4770bbaf89217e8
leobewater <mmbee888@gmail.com>
2024-03-18 10:43:24 -0400

    use pipeline to get multiple items and deserialize them

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -3,19 +3,41 @@ import { client } from '../src/services/redis';
 
 const run = async () => {
 	// Test HSET (hash set) == "HSET car color red year 1950"
-	await client.hSet('car', {
+	await client.hSet('car1', {
 		color: 'red',
-		year: 1950,
-		engine: { cylinders: 8 },
-		owner: null || '',
-		service: undefined || ''
+		year: 1950
+		// engine: { cylinders: 8 },
+		// owner: null || '',
+		// service: undefined || ''
 	});
 
-	const car = await client.hGetAll('car');
-	if (Object.keys(car).length === 0) {
-		console.log('Car not found, respond with 404');
-		return;
-	}
-	console.log(car);
+	await client.hSet('car2', {
+		color: 'green',
+		year: 1955
+	});
+
+	await client.hSet('car3', {
+		color: 'blue',
+		year: 1960
+	});
+
+	// const car = await client.hGetAll('car');
+	// if (Object.keys(car).length === 0) {
+	// 	console.log('Car not found, respond with 404');
+	// 	return;
+	// }
+	// console.log(car);
+
+	// pipeline
+	const commands = [1, 2, 3].map((id) => {
+		return client.hGetAll('car' + id);
+	});
+	const results = await Promise.all(commands);
+	// const results = await Promise.all([
+	//   client.hGetAll('car1'),
+	//   client.hGetAll('car2'),
+	//   client.hGetAll('car3'),
+	// ]);
+	console.log(results);
 };
 run();
--- src/services/queries/items/items.ts
+++ src/services/queries/items/items.ts
@@ -6,24 +6,37 @@ import type { CreateItemAttrs } from '$services/types';
 import { genId } from '$services/utils';
 
 export const getItem = async (id: string) => {
-  const item = await client.hGetAll(itemsKey(id));
-	
-  // If no items found, Redis returns an empty {}
+	const item = await client.hGetAll(itemsKey(id));
+
+	// If no items found, Redis returns an empty {}
 	if (Object.keys(item).length === 0) {
 		return null;
-  }
-  
-  return deserialize(id, item);
+	}
+
+	return deserialize(id, item);
 };
 
-export const getItems = async (ids: string[]) => {};
+export const getItems = async (ids: string[]) => {
+	const commands = ids.map((id) => {
+		return client.hGetAll(itemsKey(id));
+	});
+	const results = await Promise.all(commands);
+
+	return results.map((result, i) => {
+		if (Object.keys(result).length === 0) {
+			return null;
+		}
+
+		return deserialize(ids[i], result);
+	});
+};
 
 export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
 	const id = genId();
 
 	const serialized = serialize(attrs);
-	
-  await client.hSet(itemsKey(id), serialized);
-	
-  return id;
+
+	await client.hSet(itemsKey(id), serialized);
+
+	return id;
 };
