fcc95f5388a46dad672ca0353a6776068b4b5726
leobewater <mmbee888@gmail.com>
2024-03-18 10:16:16 -0400

    serialize createItemAttrs

--- src/services/queries/items/serialize.ts
+++ src/services/queries/items/serialize.ts
@@ -1,3 +1,11 @@
 import type { CreateItemAttrs } from '$services/types';
+import { attr } from 'svelte/internal';
 
-export const serialize = (attrs: CreateItemAttrs) => {};
+export const serialize = (attrs: CreateItemAttrs) => {
+	return {
+		...attrs,
+		// override created and updated with with unix time
+		createdAt: attrs.createdAt.toMillis(),
+		endingAt: attrs.endingAt.toMillis()
+	};
+};
